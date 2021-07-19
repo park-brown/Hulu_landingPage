a16003c458d2362733bb7b1fd678053a8bee2666
Park cloud <340688748@qq.com>
2021-07-19 14:56:34 +0800

    fix position issue and make library adapt to desktop

--- src/section/Library.jsx
+++ src/section/Library.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Box, Typography, Grid } from '@material-ui/core';
+import { Box, Typography } from '@material-ui/core';
 import { styled } from '@material-ui/core/styles';
-import { Grain } from '@material-ui/icons';
+
 const GridContainer = styled(Box, { name: 'grid-container' })(({ theme }) => ({
 	[theme.breakpoints.up('mobile')]: {
 		width: '100%',
@@ -25,6 +25,14 @@ const GridContainer = styled(Box, { name: 'grid-container' })(({ theme }) => ({
 
 		gridTemplateAreas: `"bob-burger     billie-holiday"
                         "solar-opposite   ww84"`
+	},
+	[theme.breakpoints.up('desktop')]: {
+		width: '100%',
+		maxWidth: '1276px',
+		gridTemplateColumns: 'repeat(4,minmax(294px, 1fr))',
+		gridTemplateRows: 'repeat(1, auto)',
+
+		gridTemplateAreas: `"bob-burger billie-holiday  solar-opposite   ww84"`
 	}
 }));
 const Gradient = styled(Box, { name: 'library-overlay' })(() => ({
@@ -75,112 +83,114 @@ const Library = () => {
 						and more.
 					</Typography>
 				</Box>
-				<GridContainer>
-					<Box gridArea='bob-burger' sx={{ width: '294px', height: '450px' }}>
-						<Box
-							className='item-wrapper'
-							sx={{
-								width: '100%',
-								height: '100%',
-								margin: '0 auto',
-								backgroundImage: 'url(./library/bobsburgers.jpg)',
-								backgroundSize: 'cover',
-								backgroundRepeat: 'no-repeat',
-								position: 'relative',
-								borderRadius: '8px'
-							}}>
-							<Gradient />
-							<Box className='item-title' sx={{ position: 'absolute', top: 0, left: 0, padding: '25px' }}>
-								<Typography variant='library_item_eyebrow' component='h6' sx={{ mb: '10px' }}>
-									Past & Current Seasons
-								</Typography>
-								<Typography variant='library_item_category' component='p'>
-									TV Shows
-								</Typography>
-							</Box>
-						</Box>
-					</Box>
-					<Box gridArea='billie-holiday' sx={{ width: '294px', height: '450px' }}>
-						<Box
-							className='item-wrapper'
-							sx={{
-								width: '100%',
-								height: '100%',
-								margin: '0 auto',
-								backgroundImage: 'url(./library/billieholiday.jpg)',
-								backgroundSize: 'cover',
-								backgroundRepeat: 'no-repeat',
-								position: 'relative',
-								borderRadius: '8px'
-							}}>
-							<Gradient />
-							<Box className='item-title' sx={{ position: 'absolute', top: 0, left: 0, padding: '25px' }}>
-								<Typography variant='library_item_eyebrow' component='h6' sx={{ mb: '10px' }}>
-									New & Classic
-								</Typography>
-								<Typography variant='library_item_category' component='p'>
-									Movies
-								</Typography>
-							</Box>
-						</Box>
-					</Box>
-					<Box gridArea='solar-opposite' sx={{ width: '294px', height: '450px' }}>
-						<Box
-							className='item-wrapper'
-							sx={{
-								width: '100%',
-								height: '100%',
-								margin: '0 auto',
-								backgroundImage: 'url(./library/solaropposites.jpg)',
-								backgroundSize: 'cover',
-								backgroundRepeat: 'no-repeat',
-								position: 'relative',
-								borderRadius: '8px'
-							}}>
-							<Gradient />
-							<Box className='item-title' sx={{ position: 'absolute', top: 0, left: 0, padding: '25px' }}>
-								<Typography variant='library_item_eyebrow' component='h6' sx={{ mb: '10px' }}>
-									GroundBreaking
-								</Typography>
-								<Typography variant='library_item_category' component='p'>
-									Hulu Originals
-								</Typography>
-							</Box>
-						</Box>
-					</Box>
-					<Box gridArea='ww84' sx={{ width: '294px', height: '450px' }}>
-						<Box
-							className='item-wrapper'
-							sx={{
-								width: '100%',
-								height: '100%',
-								margin: '0 auto',
-								backgroundImage: 'url(./library/WW84.jpg)',
-								backgroundSize: 'cover',
-								backgroundRepeat: 'no-repeat',
-								position: 'relative',
-								borderRadius: '8px'
-							}}>
-							<Gradient />
-							<Box className='item-title' sx={{ position: 'absolute', top: 0, left: 0, padding: '25px' }}>
-								<Typography variant='library_item_eyebrow' component='h6' sx={{ mb: '10px' }}>
-									Add-on
-								</Typography>
-								<Typography variant='library_item_category' component='p'>
-									Premiums
-								</Typography>
-							</Box>
-							<Box
-								className='item-legal'
-								sx={{ position: 'absolute', bottom: '-45px', left: '12px', right: 0, paddingTop: '25px' }}>
-								<Typography component='h6' sx={{ fontSize: '10px', lineHeight: '16px', textAlign: 'center' }}>
-									Premium network add-ons available for an additional cost
-								</Typography>
-							</Box>
-						</Box>
-					</Box>
-				</GridContainer>
 			</Box>
+			<GridContainer>
+				<Box gridArea='bob-burger' sx={{ width: '294px', height: '450px' }}>
+					<Box
+						className='item-wrapper'
+						sx={{
+							width: '100%',
+							height: '100%',
+							margin: '0 auto',
+							backgroundImage: 'url(./library/bobsburgers.jpg)',
+							backgroundSize: 'cover',
+							backgroundRepeat: 'no-repeat',
+							position: 'relative',
+							borderRadius: '8px'
+						}}>
+						<Gradient />
+						<Box className='item-title' sx={{ position: 'absolute', top: 0, left: 0, padding: '25px' }}>
+							<Typography variant='library_item_eyebrow' component='h6' sx={{ mb: '10px' }}>
+								Past & Current Seasons
+							</Typography>
+							<Typography variant='library_item_category' component='p'>
+								TV Shows
+							</Typography>
+						</Box>
+					</Box>
+				</Box>
+				<Box gridArea='billie-holiday' sx={{ width: '294px', height: '450px' }}>
+					<Box
+						className='item-wrapper'
+						sx={{
+							width: '100%',
+							height: '100%',
+							margin: '0 auto',
+							backgroundImage: 'url(./library/billieholiday.jpg)',
+							backgroundSize: 'cover',
+							backgroundRepeat: 'no-repeat',
+							position: 'relative',
+							borderRadius: '8px'
+						}}>
+						<Gradient />
+						<Box className='item-title' sx={{ position: 'absolute', top: 0, left: 0, padding: '25px' }}>
+							<Typography variant='library_item_eyebrow' component='h6' sx={{ mb: '10px' }}>
+								New & Classic
+							</Typography>
+							<Typography variant='library_item_category' component='p'>
+								Movies
+							</Typography>
+						</Box>
+					</Box>
+				</Box>
+				<Box gridArea='solar-opposite' sx={{ width: '294px', height: '450px' }}>
+					<Box
+						className='item-wrapper'
+						sx={{
+							width: '100%',
+							height: '100%',
+							margin: '0 auto',
+							backgroundImage: 'url(./library/solaropposites.jpg)',
+							backgroundSize: 'cover',
+							backgroundRepeat: 'no-repeat',
+							position: 'relative',
+							borderRadius: '8px'
+						}}>
+						<Gradient />
+						<Box className='item-title' sx={{ position: 'absolute', top: 0, left: 0, padding: '25px' }}>
+							<Typography variant='library_item_eyebrow' component='h6' sx={{ mb: '10px' }}>
+								GroundBreaking
+							</Typography>
+							<Typography variant='library_item_category' component='p'>
+								Hulu Originals
+							</Typography>
+						</Box>
+					</Box>
+				</Box>
+				<Box gridArea='ww84' sx={{ width: '294px', height: '450px' }}>
+					<Box
+						className='item-wrapper'
+						sx={{
+							width: '100%',
+							height: '100%',
+							margin: '0 auto',
+							backgroundImage: 'url(./library/WW84.jpg)',
+							backgroundSize: 'cover',
+							backgroundRepeat: 'no-repeat',
+							position: 'relative',
+							borderRadius: '8px'
+						}}>
+						<Gradient />
+						<Box className='item-title' sx={{ position: 'absolute', top: 0, left: 0, padding: '25px' }}>
+							<Typography variant='library_item_eyebrow' component='h6' sx={{ mb: '10px' }}>
+								Add-on
+							</Typography>
+							<Typography variant='library_item_category' component='p'>
+								Premiums
+							</Typography>
+						</Box>
+						<Box
+							className='item-legal'
+							sx={{ position: 'absolute', bottom: '-45px', left: '12px', right: 0, paddingTop: '25px' }}>
+							<Typography
+								component='h6'
+								sx={{ fontSize: '10px', lineHeight: '16px', textAlign: 'center', color: '#fff' }}>
+								Premium network add-ons available for an additional cost
+							</Typography>
+						</Box>
+					</Box>
+				</Box>
+			</GridContainer>
 		</Box>
 	);
 };
